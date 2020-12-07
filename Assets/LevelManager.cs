81ef4b9c943f1614b521c430cb3953927743f7d6
Marija Isupova <marija.isupova@gmail.com>
2020-12-07 21:01:48 +0200

    Change color of obstacles

---
+++ Assets/LevelManager.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelManager : MonoBehaviour
+{
+    int currentLevel;
+    int maxLevel;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentLevel = SceneManager.GetActiveScene().buildIndex;
+        maxLevel = SceneManager.sceneCount;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        int nextLevel;
+
+        if (Input.GetKey(KeyCode.L))
+        {
+            if (currentLevel < maxLevel)
+            {
+                nextLevel = currentLevel + 1;
+            }
+            else
+            {
+                nextLevel = 0;
+            }
+            
+            SceneManager.LoadScene(nextLevel);
+           
+        }
+    }
+}

9ce31278290bfa4b7c0b9bb4eed4f7ff96a228f5
Marija Isupova <marija.isupova@gmail.com>
2021-04-07 20:11:00 +0300

    Add language select window

---
+++ Assets/UI_InputWindow.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class UI_InputWindow : MonoBehaviour
+{
+    private static UI_InputWindow instance;
+
+    private Button continueBtn;
+    private TMP_InputField inputField;
+
+    private void Awake() {
+        instance = this;
+        
+        continueBtn = transform.Find("continueBtn").GetComponent<Button>();
+        inputField = transform.Find("inputField").GetComponent<TMP_InputField>();
+        
+        Hide();
+    }
+
+    void Update() {
+        // Debug.Log(inputField.text);
+    }
+
+    private void Show() {
+        gameObject.SetActive(true);
+    }
+
+    private void Hide() {
+        gameObject.SetActive(false);
+    }
+
+    public static void Show_Static() {
+        instance.Show();
+    }
+}
---
+++ Assets/UI_LanguageSelect.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UI_LanguageSelect : MonoBehaviour
+{
+    private Button englishBtn;
+
+    private Button latvianBtn;
+
+    private void Awake()
+    {
+        englishBtn = transform.Find("englishBtn").GetComponent<Button>();
+        latvianBtn = transform.Find("latvianBtn").GetComponent<Button>();
+
+        englishBtn.onClick.AddListener (TaskOnClick);
+    }
+
+    void TaskOnClick()
+    {
+        Hide();
+        UI_InputWindow.Show_Static();
+    }
+
+    public void Show()
+    {
+        gameObject.SetActive(true);
+    }
+
+    public void Hide()
+    {
+        gameObject.SetActive(false);
+    }
+}

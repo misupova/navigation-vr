b01dc64d3a58138d0903de6f83999d9bda4f2159
Marija Isupova <marija.isupova@gmail.com>
2021-04-18 22:36:02 +0300

    Add put method to node server

--- node/main.js
+++ node/main.js
@@ -5,26 +5,55 @@ var cors = require('cors')
 require('dotenv').config()
 
 const server = Express()
-server.use(BodyParser.json({ limit: '50mb' }));
-server.use(BodyParser.urlencoded({ limit: '50mb', extended: true, parameterLimit: 50000 }));
-server.use(Express.json({limit: '50mb'}));
-server.use(Express.urlencoded({limit: '50mb'}));
-server.use(cors());
+server.use(BodyParser.json({ limit: '50mb' }))
+server.use(
+  BodyParser.urlencoded({
+    limit: '50mb',
+    extended: true,
+    parameterLimit: 50000,
+  }),
+)
+server.use(Express.json({ limit: '50mb' }))
+server.use(Express.urlencoded({ limit: '50mb' }))
+server.use(cors())
+
+function clean(obj) {
+  for (var propName in obj) {
+    if (obj[propName] === null || obj[propName] === undefined || obj[propName] === "") {
+      delete obj[propName];
+    }
+  }
+  return obj
+}
 
 const client = new MongoClient(process.env.DB_HOST, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 })
 
-var collection;
+var collection
 
 server.post('/movement_data', async (request, response, next) => {
   try {
+    console.log(request.body);
     let result = await collection.insertOne(request.body)
-
     response.send(result)
   } catch (e) {
-	  console.log(e);
+    console.log(e)
+    response.status(500).send({ message: e.message })
+  }
+})
+
+server.put('/movement_data/:playerId', async (request, response, next) => {
+  try {
+    console.log(request.body);
+    let result = await collection.updateOne(
+      { playerId: request.params.playerId },
+      { $set: clean(request.body) },
+    )
+    response.send(result)
+  } catch (e) {
+    console.log(e)
     response.status(500).send({ message: e.message })
   }
 })

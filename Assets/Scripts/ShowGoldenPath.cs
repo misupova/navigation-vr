5afd0c74a7033a4c20b842e5fbc3b4a6771704c2
Marija Isupova <marija.isupova@gmail.com>
2021-05-17 21:09:26 +0300

    Add script for finding the shortest path

---
+++ Assets/Scripts/ShowGoldenPath.cs
@@ -0,0 +1,34 @@
+﻿// ShowGoldenPath
+using UnityEngine;
+using UnityEngine.AI;
+
+public class ShowGoldenPath : MonoBehaviour
+{
+    public Transform target;
+    private NavMeshPath path;
+    private NavMeshAgent agent;
+    
+    private float elapsed = 0.0f;
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+        path = new NavMeshPath();
+        elapsed = 0.0f;
+        NavMesh.CalculatePath(transform.position, target.position, NavMesh.AllAreas, path);
+        agent.SetPath(path);
+    }
+
+    void Update()
+    {
+    //     // Update the way to the goal every second.
+    //     elapsed += Time.deltaTime;
+    //     if (elapsed > 1.0f)
+    //     {
+    //         elapsed -= 1.0f;
+            
+    //     }
+    //     for (int i = 0; i < path.corners.Length - 1; i++)
+            
+    //         Debug.DrawLine(path.corners[i], path.corners[i + 1], Color.red,2, false);
+    }
+}
